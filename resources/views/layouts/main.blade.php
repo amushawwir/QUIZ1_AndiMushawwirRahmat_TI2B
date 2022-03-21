25179d8bca80ba3d78543e9b1a42b6038bf1474b
amushawwir <mushawwir123@gmail.com>
2022-03-21 16:12:49 +0800

    QUIZ1_AndiMushawwirRahmat_TI2B

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>ANDI MUSHAWWIR RAHMAT - {{ $title }}</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+<body>
+
+<div id="wrap">
+  <div class="header">
+    @yield('header')
+
+  </div>
+  <div class="center_content">
+    @yield('center_content')
+  </div>
+
+  <div class="footer">
+    @yield('footer')
+  </div>
+
+</div>
+</body>
+</html>
